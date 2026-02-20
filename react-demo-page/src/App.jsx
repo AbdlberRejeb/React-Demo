035f074eb880d6c86d3bad276498796845c07e9e
AbdlberRejeb <ethiofullstackdev@gmail.com>
2026-02-21 00:10:46 +0300

    intial commit

---
+++ react-demo-page/src/App.jsx
@@ -0,0 +1,15 @@
+import reactImage from './assets/react.svg'
+
+function App(){
+    const descriptions = ["Fundamental", " Core", "Crutial"]
+    function getRandomDescription(max){
+        return Math.floor(Math.random()*(max+1))
+    }
+    return (
+        <div>
+            
+        </div>
+      
+    )
+}
+export default App
